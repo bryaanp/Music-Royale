f781af0051908ddb3d433c8e40ee37eb09b84e80
Bryan <48010993+bryaanp@users.noreply.github.com>
2022-01-27 11:14:40 -0800

    Adding files

---
+++ App.js
@@ -0,0 +1,51 @@
+import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { LoginScreen, HomeScreen, SignupScreen } from './screens'
+import { firebase } from './firebase'
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
+
+const Stack = createStackNavigator();
+
+export default function App() {
+
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setUser(userData)
+            setLoading(false)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={user ? 'HomeScreen' : 'Login'}>
+        <Stack.Screen name="HomeScreen">
+          {props => <HomeScreen {...props} extraData={user}/>}
+        </Stack.Screen>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Registration" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+  
+}
---
+++ firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBuA_oJVdDtScr8BQYmmiPv6K_mL75Qfyg",
+  authDomain: "music-royale-a3aaa.firebaseapp.com",
+  projectId: "music-royale-a3aaa",
+  storageBucket: "music-royale-a3aaa.appspot.com",
+  messagingSenderId: "623611242673",
+  appId: "1:623611242673:web:eb1a775aec4c6806f5da90",
+  measurementId: "G-ESG5BJ6W6K"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
