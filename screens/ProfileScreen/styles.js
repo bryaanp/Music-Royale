a98a4a0f0b784d35adb0ae75de0e0c85f7c9bea3
JohnathonHeidari <JohnathonHeidari@gmail.com>
2022-02-12 15:16:13 -0800

    Fix web simulator
    one style was restricted to iOS, my bad.

--- screens/ProfileScreen/styles.js
+++ screens/ProfileScreen/styles.js
@@ -64,7 +64,7 @@ export default StyleSheet.create({
       },
       textInput: {
         flex: 1,
-        marginTop: Platform.OS === 'ios' ? 0 : -12,
+        // marginTop: Platform.OS === 'ios' ? 0 : -12,
         paddingLeft: 10,
         color: '#05375a',
       },
