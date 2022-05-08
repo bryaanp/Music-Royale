fbb2f09e4eea9bdee496edbbe3dbb3692d7b49e0
Juancy15 <80787299+Juancy15@users.noreply.github.com>
2022-05-07 23:35:08 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+//Libraries to be Installed:
+//npm install axios react-router-dom (allows Dynamic Routing in App and API Routing)
+//npm install @mui/material @emotion/react @emotion/styled (Handles UI of Quiz)
+//npm install firebase (allows firebase connectivity to app)
+
+import React from 'react'
+import './App.css';
+import Quiz from './components/Quiz';
+import PlayQuiz from './components/PlayQuiz';
+import Results from './components/Result';
+import {
+  Route,
+  Routes
+} from "react-router-dom";
+function App() {
+  return (
+    <div className='app-main'>
+      <Routes>
+        <Route exact path='/' element={<Quiz />} />
+        <Route exact path='/play' element={<PlayQuiz />} />
+        <Route exact path='/results' element={<Results />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
